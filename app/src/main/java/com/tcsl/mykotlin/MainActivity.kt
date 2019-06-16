820b920d7fca09263112a81ab2f062991acf5953
chengshenghao <e115179>
2019-06-16 14:51:54 +0800

    new：添加集合使用

--- app/src/main/java/com/tcsl/mykotlin/MainActivity.kt
+++ app/src/main/java/com/tcsl/mykotlin/MainActivity.kt
@@ -27,11 +27,9 @@ class MainActivity : AppCompatActivity() {
         //局部变量
         val c: Int// 如果没有初始值类型不能省略
         c = 3 // 明确赋值
-
         //可重新赋值的变量使用 var 关键字：
         var x = 5 // 自动推断出 `Int` 类型
         x += 1
-
         stringModel()
 
         Log.i(TAG, "" + maxOf(a, b))
@@ -43,6 +41,7 @@ class MainActivity : AppCompatActivity() {
         forsss();
 
         inUse();
+        getfruit()
     }
 
     // 定义函数
@@ -56,15 +55,14 @@ class MainActivity : AppCompatActivity() {
     /**
      *  将表达式作为函数体、返回值类型自动推断的函数
      */
-    fun sum2(a: Int, b: Int) = a + b;
+    fun sum2(a: Int, b: Int) = a + b
 
     /**
      * 函数返回无意义的值
      */
-    fun printSum(a: Int, b: Int): Unit {
-        Log.i(TAG, (a + b).toString());
+    fun printSum(a: Int,b: Int):Unit{
+        println("sum a and b is ${a+b}")
     }
-
     /**
      * Unit可以省略
      */
@@ -144,20 +142,45 @@ class MainActivity : AppCompatActivity() {
      * 使用for循环
      */
     fun forsss() {
-        for (item in items) {
-            Log.i(TAG,item)
+//        for (item in items){
+//            Log.d("for循环",item)
+//        }
+        for (index in items.indices){
+            Log.d("for循环",items[index])
         }
+
     }
 
     val m = 10
-    val n = 9
+    val n = 8
     /**
      * 使用 in 运算符来检测某个数字是否在指定区间内
      */
     fun inUse() {
         if (m in 1..n + 2) {
-            Log.i(TAG,"fits in range")
+            Log.i(TAG, "fits in range")
         }
     }
+
+    /**
+     * 集合的使用
+     */
+    val fruits = listOf("banana", "avocado", "apple")
+    fun getfruit(){
+        for (fruit in fruits){
+            Log.d(TAG,fruit)
+        }
+
+        when {
+            "orange" in items -> println("juicy")
+            "apple" in items -> println("apple is fine too")
+        }
+        //使用 lambda 表达式来过滤（filter）与映射（map）集合
+        fruits.filter { it.startsWith("a") }
+                .sortedBy { it }
+                .map { it.toUpperCase() }
+                .forEach { println(it) }
+    }
+
 
 }
